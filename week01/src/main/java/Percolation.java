4d2eb6dda32a97f708153a66b592ecba2d95d76c
Sid Zhang <zhangnaixiao@outlook.com>
2020-07-15 21:04:29 +0900

    Add comments

--- week01/src/main/java/Percolation.java
+++ week01/src/main/java/Percolation.java
@@ -1,42 +1,68 @@
-import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    /**
+     * the numbers of rows/columns of the grid
+     */
     private final int n;
-    private boolean[] grid;
-    private int openNum;
-    private final WeightedQuickUnionUF wquf;
 
+    /**
+     * n*n grid
+     */
+    private boolean[] grid;
+
+    /**
+     * how many items are opened
+     */
+    private int openNum;
+
+    /**
+     * UF now is storing a structure of size (1 + n*n + n*n + 1), which is a virtual top node, following by a n*n grid, following by n*n mirror grip, then a virtual bottom.
+     * The following diagram illustrate this structure:
+     * <p>
+     * ------- v-top --------  <- v-top is a virtual node on the top
+     * 1  2  .  .  .  .  .  n
+     * 2  .  .  .  .  .  .  n
+     * .  .  .  .  .  .  .  n
+     * .  .  .  .  .  .  .  n
+     * .  .  .  .  .  .  .  n
+     * n  .  .  .  .  .  .  n
+     * ----------------------  <- the n*n below this line is a mirror of the grid of n*n above
+     * n  .  .  .  .  .  .  n
+     * .  .  .  .  .  .  .  n
+     * .  .  .  .  .  .  .  n
+     * .  .  .  .  .  .  .  n
+     * 2  .  .  .  .  .  .  n
+     * 1  2  .  .  .  .  .  n
+     * ------ v-bottom ------  <- v-bottom is a virtual node at the very bottom
+     */
+    private final WeightedQuickUnionUF wquf;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0 || n > Integer.MAX_VALUE - 1) {
             throw new IllegalArgumentException();
         }
-        openNum = 0;
         this.n = n;
-        // top(v) ---> n*n ---> n*n(mirror) ---> bottom(v)
-        wquf = new WeightedQuickUnionUF(2 * this.n * this.n + 2);
-        grid = new boolean[this.n * this.n + 2];
-        for (int i = 0; i < grid.length; ++i) {
-            grid[i] = false;
-        }
+        wquf = new WeightedQuickUnionUF(1 + n * n + n * n + 1);
+        grid = new boolean[1 + n * n + 1];
         initFirstRow();
         initLastRow();
     }
 
+    // connect virtual top to the first row of mirror grid
     private void initFirstRow() {
-        // connect virtual top to the first row of mirror grid
         for (int i = 1; i <= n; ++i) {
             wquf.union(i, 0);
         }
     }
 
+    // connect virtual bottom to the last row of mirror grid
     private void initLastRow() {
-        // connect virtual bottom to the last row of mirror grid
-        for (int i = n * (2 * n - 1) + 1; i <= n * 2 * n; ++i) {
-            wquf.union(i, 2 * n * n + 1);
+        int mirrorStart = 1 + n * n;
+        for (int i = mirrorStart + n * (n - 1); i < mirrorStart + n * n; ++i) {
+            wquf.union(i, mirrorStart + n * n);
         }
     }
 
@@ -46,52 +72,44 @@ public class Percolation {
         }
     }
 
+    private int elem(int row, int col) {
+        return (row - 1) * n + col;
+    }
+
+    private int mirrorElem(int row, int col) {
+        int mirrorRow = 2 * n - row + 1;
+        return (mirrorRow - 1) * n + col;
+    }
+
     public void open(int row, int col) {
         validate(row, col);
-        int elem = (row - 1) * n + col;
-        int mirror = (mirrorRow(row) - 1) * n + col;
+        int elem = elem(row, col);
+        int mirrorElem = mirrorElem(row, col);
         if (grid[elem]) {
             return;
         }
         grid[elem] = true;
         openNum++;
-        if (row != 1) {
-            int up = elem - n;
-            if (grid[up]) {
-                wquf.union(up, elem);
-                wquf.union(mirror + n, mirror);
-            }
+        if (row != 1 && grid[elem - n]) { // (elem - n) -> up
+            wquf.union(elem - n, elem);
+            wquf.union(mirrorElem + n, mirrorElem);
         }
         if (row != n) {
-            int down = (row) * n + col;
-            if (grid[down]) {
-                wquf.union(down, elem);
-                wquf.union(mirror - n, mirror);
+            if (grid[row * n + col]) { // (row * n + col) -> down
+                wquf.union(row * n + col, elem);
+                wquf.union(mirrorElem - n, mirrorElem);
             }
         } else {
-            wquf.union(mirror, elem);
+            wquf.union(mirrorElem, elem);
         }
-
-        if (col != 1) {
-            int left = elem - 1;
-            if (grid[left]) {
-                wquf.union(left, elem);
-                wquf.union(mirror - 1, mirror);
-            }
+        if (col != 1 && grid[elem - 1]) { // (elem - 1) -> left
+            wquf.union(elem - 1, elem);
+            wquf.union(mirrorElem - 1, mirrorElem);
         }
-
-        if (col != n) {
-            int right = elem + 1;
-            if (grid[right]) {
-                wquf.union(right, elem);
-                wquf.union(mirror + 1, mirror);
-            }
+        if (col != n && grid[elem + 1]) { // (elem + 1) -> right
+            wquf.union(elem + 1, elem);
+            wquf.union(mirrorElem + 1, mirrorElem);
         }
-
-    }
-
-    private int mirrorRow(int row) {
-        return 2 * n - row + 1;
     }
 
     // is the site (row, col) open?
@@ -115,23 +133,5 @@ public class Percolation {
     // does the system percolate?
     public boolean percolates() {
         return wquf.find(0) == wquf.find(2 * n * n + 1);
-    }
-
-    public static void main(String[] args) {
-        Percolation percolation = new Percolation(2);
-        percolation.open(1, 1);
-        if (percolation.isFull(1, 1)) {
-            StdOut.println("Opened");
-        }
-        if (percolation.percolates()) {
-            StdOut.println("percolates.");
-        }
-        percolation.open(2, 1);
-        if (percolation.isFull(2, 1)) {
-            StdOut.println("Opened");
-        }
-        if (percolation.percolates()) {
-            StdOut.println("percolates.");
-        }
     }
 }
